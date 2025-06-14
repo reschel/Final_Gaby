3921713f4fe40ad296ecfb2bf8163ff2cf8a50eb
reschel <reschelsilva193@gmail.com>
2025-06-14 09:51:58 -0400

    Agregados los componentes Home, About, Dashboard y Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+// Importa los componentes
+import Home from './components/Home';
+import About from './components/About';
+import Deshboard from './components/Deshboard';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/componentes/About.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+  return <h1>Acerca de Nosotros</h1>;
+};
+
+export default About;
